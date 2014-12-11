70f73e83b97da817d86ef76977d0051582564b76
extragalactic <michael.l.dangelo@gmail.com>
2014-12-11 02:04:47 -0800

    added partially working mixin for view_mission changed the way mission's are pulled in missions.jade minor css changes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 /**
  * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
+ * 
+ * Start by including the middleware you want to run for every request; you can
+ * attach middleware to the pre('routes') and pre('render') events.
+ * 
+ * For simplicity, the default setup for route controllers is for each to be in
+ * its own file, and we import all the files in the /routes/views directory.
+ * 
  * Each of these files is a route controller, and is responsible for all the
  * processing that needs to happen for the route (e.g. loading data, handling
  * form submissions, rendering the view template, etc).
- *
+ * 
  * Bind each route pattern your application should respond to in the function
  * that is exported from this module, following the examples below.
- *
+ * 
  * See the Express application routing documentation for more information:
  * http://expressjs.com/api.html#app.VERB
  */
@@ -55,7 +55,7 @@ exports = module.exports = function(app) {
 
 	app.get('/mission/:mid', routes.views.view_mission.init);
 	app.get('/mission/:mid/tap/:t/', routes.views.view_mission.tap);
-  app.post('/mission/:mid/cap/', routes.views.view_mission.cap);
+	app.post('/mission/:mid/cap/', routes.views.view_mission.cap);
 	app.get('/gs/:ground_station', routes.views.view_ground_station);
 
 	app.all('/signup', routes.views.session.signup);
@@ -64,7 +64,8 @@ exports = module.exports = function(app) {
 	app.all('/reset-password/:key', routes.views.session['reset-password']);
 	app.get('/signout', routes.views.session.signout);
 
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+	// NOTE: To protect a route so that only admins can see it, use the
+	// requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
 };
