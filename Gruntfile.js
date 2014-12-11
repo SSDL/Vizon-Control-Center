88df2ea119a5ce3782b82af3b9829b1892bcc35d
EC2 Default User <ec2-user@ip-172-31-38-226.us-west-2.compute.internal>
2014-12-11 04:18:19 +0000

    Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    nodemon: {
+      dev: {
+        script: 'vizon.js',
+        options: {
+          ignore: [
+            'node_modules/**',
+            'public/**'
+          ],
+          ext: 'js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.registerTask('default', ['jshint']);
+
+};
