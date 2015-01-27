788d5953b819dc593ebda1c5791375a939f3b7f2
Arni Lehto <arni.lehto@gmail.com>
2015-01-27 12:43:56 -0800

    comments for files

--- public/js/graph/graph.js
+++ public/js/graph/graph.js
@@ -1,19 +1,18 @@
 $(function () {
-	
+	// Connect to web socket
 	var socket = io.connect('/web');
 	
-	/*socket.emit('querymissions');
 	
-	socket.on('querymissions', function(data) {
-		for (var k in data) {
-			var $newli = $(" <li role=presentation >");
-			var $newa = $("<a class=missionvar role=menuitem tabindex=-1 href=#>");
-			$newa.text( data[k].missionId + ', ' + data[k].title);
-			$newli.append($newa);
-			$("#missiondd").append($newli);
-		}
+	// If a mission is selected, get all the TAP descriptors for it
+	$("#missiondd").on('click', '.missionvar', function() {
+	  var variable = $(this).text();
+	  $("#tapmenu").empty();
+	  $("#selectedlist").empty();
+	  $("#missionddb")[0].innerText = variable; // Have the button show the selected mission
+	  socket.emit('querytaps', variable.split(',')[0]);
 	});
-	*/
+	
+	// When a 'querytaps' is returned, fill in the data dropdown menu
 	socket.on('querytaps', function(data) {
 	  for ( var k in data ) {
 	    var $newli = $( "<li role=presentation class=dropdown-header>");
@@ -23,29 +22,12 @@ $(function () {
 	      $newa.text(data[k].data[i]);
 	      $newli.append($newa);
       }
-      $("#dropdown1").append($newli);
+      $("#tapmenu").append($newli);
     }
 	});
 	
-	socket.on('querytimedata', function(data) {
-		data.series.sort(function(a, b) { 
-    	return a[0] > b[0] ? 1 : -1;
- 	  });
-	  chart1.addSeries({
-	    name: data.name,
-	    data: data.series
-	  });
-	});
-	
-	$("#missiondd").on('click', '.missionvar', function() {
-	  var variable = $(this).text();
-	  $("#dropdown1").empty();
-	  $("#selectedlist").empty();
-	  $("#missionddb")[0].innerText = variable;
-	  socket.emit('querytaps', variable.split(',')[0]);
-	});
-	
-	$("#dropdown1").on('click', '.variable', function() {
+	// If a tap data is selected, add it to the selected data list, and go query for timedata
+	$("#tapmenu").on('click', '.variable', function() {
 	  var variable = $(this).text();
 	  var tap = $("#missionddb")[0].innerText.split(',')[0] + '-' + $(this).parent().contents()[0].wholeText.split(",")[0];
 	  var $newl = $( "<li class=list-group-item>" );
@@ -56,6 +38,18 @@ $(function () {
 	  socket.emit('querytimedata', [tap, variable]);
 	});
 	
+	// when a querytimedata call is returned, graph it
+	socket.on('querytimedata', function(data) {
+		data.series.sort(function(a, b) { 
+    	return a[0] > b[0] ? 1 : -1;
+ 	  });
+	  chart1.addSeries({
+	    name: data.name,
+	    data: data.series
+	  });
+	});
+	
+	// If an item is removed from the selected data list it flows through here
 	$("#selectedlist").on('click', '.rmvbutton', function() {
 	  for(var i = chart1.series.length - 1; i > -1; i--)
 	  {
@@ -115,52 +109,7 @@ $(function () {
 		yAxis: {
 			title: {
 				text: 'Value'
-			}/*,
-			minorGridLineWidth: 0,
-			gridLineWidth: 0,
-			tickWidth: 1,
-			alternateGridColor: null,
-			plotBands: [{ // Dead
-				from: 2,
-				to: 2.9,
-				color: 'rgba(255, 0, 0, 0.2)',
-				label: {
-					text: 'Dead',
-					style: {
-						color: '#606060'
-					}
-				}
-			}, { // Nearly Dead
-				from: 2.9,
-				to: 3.65,
-				color: 'rgba(255, 150, 0, 0.2)',
-				label: {
-					text: 'Nearly Dead',
-					style: {
-						color: '#606060'
-					}
-				}
-			}, { // Partially Charged
-				from: 3.65,
-				to: 4.13,
-				color: 'rgba(255, 255, 0, 0.2)',
-				label: {
-					text: 'Partially Charged',
-					style: {
-						color: '#606060'
-					}
-				}
-			}, { // Fully Charged
-				from: 4.13,
-				to: 4.3,
-				color: 'rgba(0, 255, 0, 0.2)',
-				label: {
-					text: 'Fully Charged',
-					style: {
-						color: '#606060'
-					}
-				}
-			}]*/
+			}
 		},
 		navigator: {
 			enabled: true
@@ -168,6 +117,8 @@ $(function () {
 
 	});
 	
+	// This turns the data plotting from line to scatter
+	// When new data is added it should default to the same as the rest
 	$('#scatterbutton').click(function() {
 		if ( this.innerHTML == 'Scatter') {
 			this.innerHTML = 'Line';
@@ -185,7 +136,8 @@ $(function () {
 			}
 		}
 	});
-		
+	
+	// Export button allows data in the current chart window to be exported to a CSV
 	$('#exportbutton').click(function () {
 		var data = chart1.getCSV();
 		var a         = document.createElement('a');
